d27b791e5fc42517f30170bdc6637208bcf49287
youngmonero <yungmonero29@gmail.com>
2025-11-06 20:55:10 +0100

    complete

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-document.getElementById('shakeButton').addEventListener('click', () => {
-  const numInput = document.getElementById('numberInput')
-  const num = parseInt(numInput.value);
+document.getElementById('shakebutton').addEventListener('click', () => {
+  const numInput = document.getElementById('numberinput')
+  const num = parseInt(numInput.value)
   const resultDiv = document.getElementById('result')
   resultDiv.innerHTML = ''
 
@@ -11,13 +11,11 @@ document.getElementById('shakeButton').addEventListener('click', () => {
 
   const actions = []
 
-
   if (num & 1) actions.push({ name: 'wink', class: 'wink' });
   if (num & 2) actions.push({ name: 'double blink', class: 'double-blink' })
   if (num & 4) actions.push({ name: 'close your eyes', class: 'close-eyes' })
   if (num & 8) actions.push({ name: 'jump', class: 'jump' })
   if (num & 16) actions.reverse()
-
 
   if (actions.length === 0) {
     resultDiv.innerHTML = '<p>No secret handshake for this number </p>'
